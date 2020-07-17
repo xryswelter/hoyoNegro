4e3bed921c31c0a4539ebcd5d7b8dd9a4a8a6a51
Andrew Oltman <aoltman@ucsd.edu>
2020-07-17 11:07:55 -0700

    Sounds added

--- Assets/User Controls/InputActions.cs
+++ Assets/User Controls/InputActions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/InputActions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/User Controls/InputActions.inputactions'
 
 using System;
 using System.Collections;
